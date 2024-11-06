fa06141266e9f94184307e23e488f96c78f3e60d
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2024-11-06 14:42:50 +0200

    Fixed the error.

--- Bill3.java
+++ Bill3.java
@@ -8,6 +8,6 @@ public class Bill3 {
 		double bill = Double.parseDouble(args[3]);
 		double split = Math.ceil(bill/3);
 
-		System.out.printf("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay " + split + " Shekels each.");
+		System.out.printf("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + split + " Shekels each.");
 	}
 }
